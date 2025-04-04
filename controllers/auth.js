111f6e974b3d9bb3777b008c0f6db2ab44feb27c
Chanya S <chanyasean.ph@gmail.com>
2025-04-05 06:09:26 +0700

    Add API Security and Creating Swagger

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,6 +34,8 @@ exports.register = async(req,res,next)=>
 //@access Public
 exports.login = async (req,res,next) => 
 {
+    try
+    {
         const {email, password} = req.body;
 
         //Validate email & pasword
@@ -56,6 +58,9 @@ exports.login = async (req,res,next) =>
         //const token = user.getSignedJwtToken();
         //return res.status(200).json({success:true,token});
         sendTokenResponse(user,200,res);
+    }catch(err) {
+        return res.status(401).json({success:false, msg:'Cannot convert email or password to string'});
+    }
 };
 
 //Get token from model, create cookie and send response
@@ -95,3 +100,23 @@ exports.getMe = async(req,res,next) =>
         }
     );
 };
+
+//@desc Log user out / clear cookie
+//@route GET/api/v1/auth/logout
+//@access Private
+exports.logout = async(req,res,next) =>
+    {
+        res.cookie('token','none',
+            {
+                expires: new Date(Date.now() + 10*1000),
+                httpOnly: true
+            }
+        );
+
+        res.status(200).json(
+            {
+                sucess: true,
+                data: {}
+            }
+        );
+    };
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss} = require('express-xss-sanitizer');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -18,6 +26,19 @@ const auth = require('./routes/auth');
 
 //Body Parser
 app.use(express.json());
+app.use(mongoSanitize()); // Sanitize Data
+app.use(xss()); // Prevent XSS attacks
+app.use(helmet()); // Set security headers
+//Rate Limiting
+const limiter = rateLimit.rateLimit(
+    {
+        windowsMs : 10*60*1000, //10 mins
+        max: 100
+    }
+);
+app.use(limiter);
+app.use(hpp()); //Prevent http param pollutions
+app.use(cors()); // Enable CORS
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/appointments',appointments);
 app.use('/api/v1/auth',auth);
@@ -34,3 +55,24 @@ process.on('unhandledRejection',(err,promise)=>{
     //Close server & exit process
     server.close(()=>process.exit(1));
 });
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info:
+        {
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'A simple Express VacQ API'
+        },
+        servers: [
+            {
+                url: 'http://localhost:5003/api/v1'
+            }
+        ],
+    },
+    apis:['./routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
